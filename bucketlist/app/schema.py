b520865ddf35a95cbc8ead3b2d6321cdb6782e51
Daisy Macharia <daisymacharia@Daisys-MacBook-Pro.local>
2017-07-02 12:21:05 +0300

    [Feature #148194963] Add validations for registration data

---
+++ bucketlist/app/schema.py
@@ -0,0 +1,16 @@
+from marshmallow import Schema, fields, validate
+
+
+class UserRegisterSchema(Schema):
+
+    fullnames = fields.String(validate=[validate.Length(min=3),
+                              validate.Regexp(r"[a-zA-Z0-9_\- ]*$",
+                                              error="Invalid characters")],
+                              required=True,
+                              error_messages={"required": "Enter full names"})
+    email = fields.Email(validate=[validate.Length(max=50)],
+                         required=True,
+                         error_messages={"required": "Enter email"})
+    password = fields.String(validate=[validate.Length(min=5)],
+                             required=True,
+                             error_messages={"required": "Enter password"})
