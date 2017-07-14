8fb4521aecb194311897173bcfe02a0d9ffbdc66
Daisy Macharia <daisymacharia@Daisys-MacBook-Pro.local>
2017-07-14 10:43:53 +0300

    set config_name

--- bucketlist/app/__init__.py
+++ bucketlist/app/__init__.py
@@ -25,7 +25,7 @@ template = {
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     Swagger(app, template=template)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config[DevelopmentConfig])
     app.config.from_pyfile('config.py', silent=True)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
