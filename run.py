6ac5b0ae72b114155e92ff1c7dd51cf3a821b1d6
Daisy Macharia <daisymacharia@Daisys-MacBook-Pro.local>
2017-07-14 10:17:27 +0300

    fix import error

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import os.path
+import sys
+from inspect import getsourcefile
 
-from .bucketlist.app import create_app
-import os
+current_path = os.path.abspath(getsourcefile(lambda: 0))
+current_dir = os.path.dirname(current_path)
+parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
+
+sys.path.insert(0, parent_dir)
+from bucketlist.app import create_app
 
 app = Flask(__name__)
 config_name = os.getenv('APP_SETTINGS')
